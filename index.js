64ef6def092ce8c73ba2a8a43c2d276ef46bb6d2
Saga <saga.helgadottir@physics.gu.se>
2020-02-11 13:58:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,12 @@ const ldl = document.forms["frm1"]["ldl"].value;
 const hdl = document.forms["frm1"]["hdl"].value;
 const tg = document.forms["frm1"]["tg"].value;
 
-console.log(age);
-
-// update variable text
-age.innerHTML = "this is age";
-
 
 
 (async () => {
   const model = await tf.loadLayersModel('https://hsaga.github.io/FH/tfjs_model/model.json');
 
-  console.log(model);
+  console.log(age);
 
   let prediction =  model.predict(tf.tensor2d([[age, ldl, hdl]]));
   prediction.print();
