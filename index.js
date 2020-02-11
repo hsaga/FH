e0d612e2ef1e1b320a71a023c086db0a56db34d5
Saga <saga.helgadottir@physics.gu.se>
2020-02-11 14:18:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 
 
 function makePrediction(){
-const age = document.forms["frm1"]["age"].value;
-const ldl = document.forms["frm1"]["ldl"].value;
-const hdl = document.forms["frm1"]["hdl"].value;
-const tg = document.forms["frm1"]["tg"].value;
+var age = document.forms["frm1"]["age"].value;
+var ldl = document.forms["frm1"]["ldl"].value;
+var hdl = document.forms["frm1"]["hdl"].value;
+var tg = document.forms["frm1"]["tg"].value;
 
+const age = Number(age);
+const ldl = Number(ldl);
+const hdl = Number(hdl);
+const tg = Number(tg);
 
+document.getElementById("variables").innerHTML = age;
 
 (async () => {
   const model = await tf.loadLayersModel('https://hsaga.github.io/FH/tfjs_model/model.json');
@@ -14,8 +19,8 @@ const tg = document.forms["frm1"]["tg"].value;
   console.log(typeof age);
   console.log(model);
 
-  /*let prediction =  model.predict(tf.tensor2d([[age, ldl, hdl]]));
-  prediction.print();*/
+  let prediction =  model.predict(tf.tensor2d([[age, ldl, hdl]]));
+  prediction.print();
 
 })();
 
