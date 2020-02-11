da97a1172f16bb8189fd2669976365c33e4c7180
Saga <saga.helgadottir@physics.gu.se>
2020-02-11 11:12:21 +0100

    upload files

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+(async () => {
+  const model = await tf.loadLayersModel('https://hsaga.github.io/FH/tfjs_model/model.json');
+
+  console.log(model);
+
+  let prediction =  model.predict(tf.tensor2d([[age, ldl, hdl]]));
+  prediction.print();
+
+})();
+
+
+
+
+
+function makePrediction() {
+  
+  const age = document.forms["frm1"]["age"].value;
+  const ldl = document.forms["frm1"]["ldl"].value;
+  const hdl = document.forms["frm1"]["hdl"].value;
+  const tg = document.forms["frm1"]["tg"].value;
+
+  console.log(age);
+
+}
+
+  
