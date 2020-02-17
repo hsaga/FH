bf574c7869048f5bdbf2c3ef02d8568b6b08c9c1
Saga <saga.helgadottir@physics.gu.se>
2020-02-17 14:39:50 +0100

    update

--- index.js
+++ index.js
@@ -73,6 +73,20 @@ function makePrediction(){
 
 }
 
+function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "predictfh@gmail.com",
+	Password : "fhprediction",
+	To : '<recipient’s email address>',
+	From : "<sender’s email address>",
+	Subject : "<email subject>",
+	Body : "<email body>",
+	}).then(
+		message => alert("mail sent successfully")
+	);
+}
+
 
 
   
