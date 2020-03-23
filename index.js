fe4ca584e2894cf8fdb91e6f118230a57f7397bd
Saga <saga.helgadottir@physics.gu.se>
2020-03-23 15:11:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function makePrediction(){
         yValueFormatString: " ",
         indexLabel: Math.round(predictionArray[0][0] * 1e2) / 1e2,
         legendText: " ",
-        toolTipContent: " ",
+        toolTipContent: "{y}",
         dataPoints: [
           {x: 0.5, y:  Math.round(predictionArray[0][0] * 1e2) / 1e2},
         ]
