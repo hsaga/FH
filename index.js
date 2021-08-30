63b6225ac4d4652c70112e59f463313723d45e4d
Saga <saga.helgadottir@physics.gu.se>
2021-08-30 19:13:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ function makePrediction(){
       predictionArray8 = prediction8.arraySync();
       predictionArray9 = prediction9.arraySync();
 
-      let meanPrediction =  Math.round(predictionArray[0][0]*1e2)/1e2; /* + Math.round(predictionArray1[0][0]*1e2)/1e2 ) / 2; */
+      let meanPrediction =  Math.round(predictionArray5[0][0]*1e2)/1e2; /* + Math.round(predictionArray1[0][0]*1e2)/1e2 ) / 2; */
 
 
       document.getElementById("NNprediction").innerHTML = 'Neural network prediction: '.bold() + meanPrediction;
